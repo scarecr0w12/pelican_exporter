39439a5e5c585f996f066d1c6d648918a50e7f10
Jacob Bowen <jacob@thecorehosting.net>
2025-04-06 02:06:20 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup, find_packages
 import pelican_exporter
 
+INSTALL_REQUIRES = [
+    "PyYAML~=6.0.1",
+    "setuptools~=69.1.1",
+    "python-dateutil~=2.9.0.post0",
+    "prometheus_client~=0.20.0",
+    "dataclasses_json~=0.6.4",
+    "requests~=2.31"
+]
+
 setup(
     name='pelican_exporter',
     version=pelican_exporter.__version__,
@@ -15,5 +24,7 @@ setup(
             'console_scripts': [
                 'pelican_exporter=pelican_exporter.pelican_exporter:main'
             ]
-        }
+        },
+    pythong_requires=">=3.11",
+    install_requires=INSTALL_REQUIRES
 )
